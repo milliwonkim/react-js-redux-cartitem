293659d10449fba72760a964e27b6973eed2037f
kiwon kim <rldnjs9347@gmail.com>
2020-06-11 17:29:36 +0900

    Updates

---
+++ precache-manifest.fc8a4a4f2011b24cef8f8808923a2096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28471a3339121ed6a5fe6a25212cd192",
+    "url": "/secondhand-apple-shop/index.html"
+  },
+  {
+    "revision": "e3bfe454a44436059623",
+    "url": "/secondhand-apple-shop/static/css/main.d4813631.chunk.css"
+  },
+  {
+    "revision": "c892e2badaf1ae4a0e38",
+    "url": "/secondhand-apple-shop/static/js/2.a69c65c9.chunk.js"
+  },
+  {
+    "revision": "e3bfe454a44436059623",
+    "url": "/secondhand-apple-shop/static/js/main.d0057bc1.chunk.js"
+  },
+  {
+    "revision": "4dabbeb0e6f973aab4f1",
+    "url": "/secondhand-apple-shop/static/js/runtime~main.740a4c3f.js"
+  }
+]);
